a4b2ae87b423ddb983687581dfd8a7830636de25
eldo88 <leegjones88@gmail.com>
2025-02-13 19:06:43 -0700

    Added exception handling

--- RouteTickrAPI/src/Services/AuthService.cs
+++ RouteTickrAPI/src/Services/AuthService.cs
@@ -21,43 +21,59 @@ public class AuthService : IAuthService
 
     public async Task<ServiceResult<string>> Login(LoginRequestDto request)
     {
-        var user = await _userRepository.GetUserByUsernameAsync(request.Username);
-        if (user == null || !BCrypt.Net.BCrypt.Verify(request.Password, user.PasswordHash))
-            return ServiceResult<string>.ErrorResult("User Unauthorized");
-            
-        var claims = new[]
+        try
         {
-            new Claim(JwtRegisteredClaimNames.Sub, user.Username),
-            new Claim(ClaimTypes.Role, user.Role)
-        };
+            var user = await _userRepository.GetUserByUsernameAsync(request.Username);
+            if (user == null || !BCrypt.Net.BCrypt.Verify(request.Password, user.PasswordHash))
+                return ServiceResult<string>.ErrorResult("User Unauthorized");
+            
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, user.Username),
+                new Claim(ClaimTypes.Role, user.Role)
+            };
 
-        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"] ?? string.Empty));
-        var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
-        var token = new JwtSecurityToken(
-            issuer: _config["Jwt:Issuer"],
-            audience: _config["Jwt:Audience"],
-            claims: claims,
-            expires: DateTime.UtcNow.AddHours(1),
-            signingCredentials: creds
-        );
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"] ?? string.Empty));
+            var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+            var token = new JwtSecurityToken(
+                issuer: _config["Jwt:Issuer"],
+                audience: _config["Jwt:Audience"],
+                claims: claims,
+                expires: DateTime.UtcNow.AddHours(1),
+                signingCredentials: credentials
+            );
 
-        var successToken = new JwtSecurityTokenHandler().WriteToken(token);
+            var successToken = new JwtSecurityTokenHandler().WriteToken(token);
         
-        return ServiceResult<string>.SuccessResult(successToken);
+            return ServiceResult<string>.SuccessResult(successToken);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error occured in Login {ex.Message}");
+            return ServiceResult<string>.ErrorResult("Unexpected error occured.");
+        }
     }
 
     public async Task<ServiceResult<UserDto>> Register(LoginRequestDto request)
     {
-        var existingUser = await _userRepository.GetUserByUsernameAsync(request.Username);
-        if (existingUser is not null)
-            return ServiceResult<UserDto>.ErrorResult("Username already exists");
+        try
+        {
+            var existingUser = await _userRepository.GetUserByUsernameAsync(request.Username);
+            if (existingUser is not null)
+                return ServiceResult<UserDto>.ErrorResult("Username already exists");
 
-        var newUser = UserMapper.ToUser(request);
-        var isAdded = await _userRepository.AddUserAsync(newUser);
+            var newUser = UserMapper.ToUser(request);
+            var isAdded = await _userRepository.AddUserAsync(newUser);
 
-        if (!isAdded) return ServiceResult<UserDto>.ErrorResult("Error Registering User");
-        var addedUser = UserMapper.ToUserDto(newUser);
+            if (!isAdded) return ServiceResult<UserDto>.ErrorResult("Error Registering User");
+            var addedUser = UserMapper.ToUserDto(newUser);
         
-        return ServiceResult<UserDto>.SuccessResult(addedUser);
+            return ServiceResult<UserDto>.SuccessResult(addedUser);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error occured in Register {ex.Message}");
+            return ServiceResult<UserDto>.ErrorResult("Unexpected error occured.");
+        }
     }
 }
