bcf0a8324410eb480bed6074758d3d884f676850
eldo88 <leegjones88@gmail.com>
2025-02-22 16:39:42 -0700

    Added json polymorphic type discriminators to base class

--- RouteTickrAPI/src/Entities/Climb.cs
+++ RouteTickrAPI/src/Entities/Climb.cs
@@ -1,8 +1,15 @@
 using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 using RouteTickrAPI.Enums;
 
 namespace RouteTickrAPI.Entities;
 
+[JsonPolymorphic(TypeDiscriminatorPropertyName = "ClimbType")]
+[JsonDerivedType(typeof(SportRoute), typeDiscriminator: "Sport")]
+[JsonDerivedType(typeof(TradRoute), typeDiscriminator: "Trad")]
+[JsonDerivedType(typeof(Boulder), typeDiscriminator: "Boulder")]
+[JsonDerivedType(typeof(IceClimb), typeDiscriminator: "Ice")]
+[JsonDerivedType(typeof(AlpineRockRoute), typeDiscriminator: "Alpine")]
 public abstract class Climb
 {
     [Key]
@@ -13,5 +20,5 @@ public abstract class Climb
     public string Url { get; set; } = string.Empty;
     public decimal Height { get; set; }
     public ClimbDangerRating DangerRating { get; set; }
-    public ClimbType Type { get; set; }
+    public ClimbType ClimbType { get; set; }
 }
