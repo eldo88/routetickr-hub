b679e2bfa56371268abeec9671e50b6b27dc529f
eldo88 <leegjones88@gmail.com>
2025-03-26 18:47:43 -0600

    Added additional client url for cors

--- RouteTickrAPI/Program.cs
+++ RouteTickrAPI/Program.cs
@@ -41,7 +41,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowSpecificOrigin", policy =>
     {
-        policy.WithOrigins("http://localhost:8080"/*, "http://localhost:5672"*/)
+        policy.WithOrigins("http://localhost:8080", "http://localhost:5500", "http://127.0.0.1:5500")
             .AllowAnyHeader()
             .AllowAnyMethod();
     });
